7015c10199478dd47a80ae05e768f4743f7be65b
Cesar Orozco Manotas <154420733+cesarorozcom@users.noreply.github.com>
2025-10-24 00:19:17 +0000

    Update settings

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -22,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&#hyleuabu@^i948z%%#ku5ymb-1jp7ne0fjyo+==2mtw6by04'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-&#hyleuabu@^i948z%%#ku5ymb-1jp7ne0fjyo+==2mtw6by04')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+
+    'https://finapp-production-f0ee.up.railway.app/'
+]
 
 
 # Application definition
